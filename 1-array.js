ac0f79abad2b6cfe3c12d09bdba079d735315f93
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2024-05-14 09:56:28 -0300

    Add files via upload

---
+++ 1-array.js
@@ -0,0 +1,5 @@
+let produtos = ['laranja',"maça", ' banana']
+for (let i=0; i < produtos.length; i++)
+{
+    console.log(produtos[i])
+}
---
+++ 2-array-push-unshift.js
@@ -0,0 +1,15 @@
+//definindo um array vazio de frutas 
+var frutas = [];
+
+//adicionando elementos ao final do array usando o metodo push
+
+frutas.push('maça');
+frutas.push('banana');
+frutas.push('laranja');
+
+console.log('Array após adicionar elementos ao final:', frutas);//saida
+
+frutas.unshift('pêssego');
+frutas.unshift('abacaxi');
+
+console.log('Array apos adicionar elementos ao unico:',frutas);
