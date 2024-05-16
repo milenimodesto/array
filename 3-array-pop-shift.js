41e1a197691e1755eb54bf8b7332c46a3d1ab3fb
milenimodesto <140817914+milenimodesto@users.noreply.github.com>
2024-05-16 11:55:58 -0300

    Add files via upload

---
+++ 3-array-pop-shift.js
@@ -0,0 +1,11 @@
+  var frutas = ['Maça', 'Banana','Laranja','Pêssego','Abacaxi'];
+
+  var ultimaFruta = frutas.pop();
+
+console.log('Array após remover o último elemento:', frutas);
+console.log('Último fruta removida:', ultimaFruta );
+
+  var primeiraFruta = frutas.shift();
+
+console.log('Array após remover o primeiro elemento', frutas);
+console.log('Primeira fruta removida', primeiraFruta);
